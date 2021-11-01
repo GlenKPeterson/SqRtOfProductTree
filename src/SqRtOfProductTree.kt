3413ef9b8a5ca27b77cf19cba01494a49242dfcc
Glen K. Peterson <glen.k.peterson@gmail.com>
2021-11-01 16:56:50 -0400

    Simpler and works better.

--- src/SqRtOfProductTree.kt
+++ src/SqRtOfProductTree.kt
@@ -1,13 +1,28 @@
 import kotlin.math.sqrt
 
 /**
-It's a DAG where each parent has 0 or 2 children and children have 0, 1, or 2 parents, it's built evenly, and it's 4 nodes deep.
+Problem:
+The blocks in the diagram below are arranged so that the value in each block is the square root
+of the product of the two values below it.  Find the number in the top block. Give an exact answer.
+
+               .----------.
+               |          |
+          .----------.----------.
+          |    60    |          |
+     .----------.----------.----------.
+     |          |    36    |          |
+.----------.----------.----------.----------.
+|    625   |          |          |   1296   |
+'----------'----------'----------'----------'
+
+This can be represented as a tree where each parent has 0 or 2 children.
+It's built evenly, and it's 4 nodes deep.
 
 There are several problems here:
-Represent the data.  Object-oriented programming suggests this has to go first.  Clojure programmers might not start here.
-Solve a Triple: Solve a parent and 2 children when you have values for 2 out of 3 of them.
-Traverse the tree until all groups of 3 nodes are solved (and stop traversing)
-Present the output
+ - Represent the data.  Object-oriented programming suggests this has to go first.  Clojure programmers might not start here.
+ - Solve a Triple: Solve a parent and 2 children when you have values for 2 out of 3 of them.
+ - Traverse the tree until all groups of 3 nodes are solved (and stop traversing)
+ - Present the output
 
 Of these, Solve a Triple seems the most obvious.  Given Parent p and Child 1 c1 and Child 2 c2:
 
@@ -15,26 +30,23 @@ p = root(c1, c2)
 c1 = p^2/c2
 c2 = p^2/c1
 
-I can think of 2 ways to traverse a triple:
-doubly-link the triples, so that each each parent knows how to find its children and each child its parent.  OR
-Stop from the top and use a LIFO stack to store pairs of Parent and Child-number that you're currently visiting.
-I've had real trouble implementing the LIFO traversal, so I'd go with double-linking.
+I can think of 3 ways to traverse the tree:
+ - Doubly-link the Nodes, so that each each parent knows how to find its children and each child its parent.
+ - Stop from the top and use a LIFO stack to store pairs of Parent and Child-number that you're currently visiting.
+   I've had real trouble implementing the LIFO traversal, so I'd go with double-linking.
+ - Throw the nodes in a list and don't worry about it.
 
 OK, Now I'm ready to think about representation.
+
+ At first I thought I'd have to store the left and right parents, but after
+ throwing them all in a list, my compiler told me I didn't need that.
 */
 
 class Node(
-    var leftParent: Node?,
-    var rightParent: Node?,
-    var value: Double? = null,
     var leftChild: Node? = null,
+    var value: Double? = null,
     var rightChild: Node? = null,
-) {
-    fun children(left: Node, right: Node) {
-        leftChild = left
-        rightChild = right
-    }
-}
+)
 
 /**
  * Making the nodes is hostile to a functional style, so here's the imperative way.
@@ -48,22 +60,22 @@ class Node(
  *              /  \   /   \   /   \
  * L3        l3n1    l3n2    l3n3    l3n4
  */
-val root = Node(null, null)
-
-// Level 1 below root
-val l1n1 = Node(null, root, 60.0)
-val l1n2 = Node(root, null)
+// Level 3
+val l3n1 = Node(null, 625.0, null)
+val l3n2 = Node(null, null, null)
+val l3n3 = Node(null, null, null)
+val l3n4 = Node(null, 1296.0, null)
 
 // Level 2
-val l2n1 = Node(null, l1n1)
-val l2n2 = Node(l1n1, l1n2, 36.0)
-val l2n3 = Node(l1n2, null)
+val l2n1 = Node(l3n1, null, l3n2)
+val l2n2 = Node(l3n2, 36.0, l3n3)
+val l2n3 = Node(l3n3, null, l3n4)
 
-// Level 3
-val l3n1 = Node(null, l2n1, 625.0)
-val l3n2 = Node(l2n1, l2n2)
-val l3n3 = Node(l2n2, l2n3)
-val l3n4 = Node(l2n3, null, 1296.0)
+// Level 1 below root
+val l1n1 = Node(l2n1, 60.0, l2n2)
+val l1n2 = Node(l2n2, null, l2n3)
+
+val root = Node(l1n1, null, l1n2)
 
 val allNodes: List<Node> = listOf(
     root,
@@ -108,6 +120,10 @@ fun pretty(x: Double?): String = if (x == null) {
     }
 }
 
+/**
+ * When we have 2 out of the 3 data points (parent and 2 children)
+ * we can solve for the third.
+ */
 fun solveThirdNode(currNode: Node) {
     // Do we have 2/3 data points?
     var numDataPoints = 0
@@ -136,19 +152,8 @@ fun solveThirdNode(currNode: Node) {
 }
 
 fun main() {
-    // Link the parents to the children
-    root.children(l1n1, l1n2)
-
-    l1n1.children(l2n1, l2n2)
-    l1n2.children(l2n2, l2n3)
-
-    l2n1.children(l3n1, l3n2)
-    l2n2.children(l3n2, l3n3)
-    l2n3.children(l3n3, l3n4)
-
     println("\nProblem:")
     printTree()
-
 
     // Visit each node once, top to bottom
     allNodes.forEach { solveThirdNode(it) }

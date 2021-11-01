d8462ed581f8c23c4a8cf6426e70a5de4318f218
Glen K. Peterson <glen.k.peterson@gmail.com>
2021-11-01 16:59:38 -0400

    Prettier

--- src/SqRtOfProductTree.kt
+++ src/SqRtOfProductTree.kt
@@ -1,46 +1,53 @@
 import kotlin.math.sqrt
 
 /**
-Problem:
-The blocks in the diagram below are arranged so that the value in each block is the square root
-of the product of the two values below it.  Find the number in the top block. Give an exact answer.
-
-               .----------.
-               |          |
-          .----------.----------.
-          |    60    |          |
-     .----------.----------.----------.
-     |          |    36    |          |
-.----------.----------.----------.----------.
-|    625   |          |          |   1296   |
-'----------'----------'----------'----------'
-
-This can be represented as a tree where each parent has 0 or 2 children.
-It's built evenly, and it's 4 nodes deep.
-
-There are several problems here:
- - Represent the data.  Object-oriented programming suggests this has to go first.  Clojure programmers might not start here.
- - Solve a Triple: Solve a parent and 2 children when you have values for 2 out of 3 of them.
- - Traverse the tree until all groups of 3 nodes are solved (and stop traversing)
- - Present the output
-
-Of these, Solve a Triple seems the most obvious.  Given Parent p and Child 1 c1 and Child 2 c2:
-
-p = root(c1, c2)
-c1 = p^2/c2
-c2 = p^2/c1
-
-I can think of 3 ways to traverse the tree:
- - Doubly-link the Nodes, so that each each parent knows how to find its children and each child its parent.
- - Stop from the top and use a LIFO stack to store pairs of Parent and Child-number that you're currently visiting.
-   I've had real trouble implementing the LIFO traversal, so I'd go with double-linking.
- - Throw the nodes in a list and don't worry about it.
-
-OK, Now I'm ready to think about representation.
-
- At first I thought I'd have to store the left and right parents, but after
- throwing them all in a list, my compiler told me I didn't need that.
-*/
+ * Problem:
+ * The blocks in the diagram below are arranged so that the value in each
+ * block is the square root of the product of the two values below it.
+ * Find the number in the top block. Give an exact answer.
+ *
+ *                .----------.
+ *                |          |
+ *           .----------.----------.
+ *           |    60    |          |
+ *      .----------.----------.----------.
+ *      |          |    36    |          |
+ * .----------.----------.----------.----------.
+ * |    625   |          |          |   1296   |
+ * '----------'----------'----------'----------'
+ *
+ * This can be represented as a tree where each parent has 0 or 2 children.
+ * It's built evenly, and it's 4 nodes deep.
+ *
+ * There are several problems here:
+ *  - Represent the data.  Object-oriented programming suggests this has to go
+ *    first.  Clojure programmers might not start here.
+ *  - Solve a Triple: Solve a parent and 2 children when you have values for 2
+ *    out of 3 of them.
+ *  - Traverse the tree until all groups of 3 nodes are solved (and stop
+ *    traversing)
+ *  - Present the output
+ *
+ * Of these, Solve a Triple seems the most obvious.  Given Parent p and Child
+ * 1 c1 and Child 2 c2:
+ *
+ * p = root(c1, c2)
+ * c1 = p^2/c2
+ * c2 = p^2/c1
+ *
+ * I can think of 3 ways to traverse the tree:
+ *  - Doubly-link the Nodes, so that each each parent knows how to find its
+ *    children and each child its parent.
+ *  - Stop from the top and use a LIFO stack to store pairs of Parent and
+ *    Child-number that you're currently visiting. I've had real trouble
+ *    implementing the LIFO traversal, so I'd go with double-linking.
+ *  - Throw the nodes in a list and don't worry about it.
+ *
+ * OK, Now I'm ready to think about representation.
+ *
+ *  At first I thought I'd have to store the left and right parents, but after
+ *  throwing them all in a list, my compiler told me I didn't need that.
+ */
 
 class Node(
     var leftChild: Node? = null,
@@ -49,8 +56,7 @@ class Node(
 )
 
 /**
- * Making the nodes is hostile to a functional style, so here's the imperative way.
- * DAG looks like:
+ * DAG looks like this:
  *
  *                      root
  *                      /  \
@@ -59,6 +65,8 @@ class Node(
  * Level 2      l2n1    l2n2    l2n3
  *              /  \   /   \   /   \
  * L3        l3n1    l3n2    l3n3    l3n4
+ *
+ * We'll build it from the bottom up
  */
 // Level 3
 val l3n1 = Node(null, 625.0, null)
